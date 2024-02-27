04c76726e5c769e20c3fbefff0a573aae3a0108e
NathanNux <139380259+NathanNux@users.noreply.github.com>
2024-02-27 17:25:06 +0100

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,20 @@
+import Head from 'next/head';
 import { Inter } from "next/font/google";
 import "./global.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Centrum Locomotive Scroll LP",
+  title: "Centrum Locomotive Scroll",
   description: "Locomotive scroll landing page",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
